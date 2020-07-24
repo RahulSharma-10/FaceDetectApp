686045dd4961a057b5606132441d8269f5feec56
Rahul Sharma <rahul.10.srm@gmail.com>
2020-07-24 17:50:34 +0530

    Updates

---
+++ precache-manifest.ca10ab051bdfd915d418006be356d28f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef4ef19a5cc802685912186b8ea7cec",
+    "url": "/FaceDetectApp/index.html"
+  },
+  {
+    "revision": "fcb37d17582d6514fefd",
+    "url": "/FaceDetectApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd7db376b3b9d908555e",
+    "url": "/FaceDetectApp/static/css/main.ee7441ab.chunk.css"
+  },
+  {
+    "revision": "fcb37d17582d6514fefd",
+    "url": "/FaceDetectApp/static/js/2.11a31118.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceDetectApp/static/js/2.11a31118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7db376b3b9d908555e",
+    "url": "/FaceDetectApp/static/js/main.401ce536.chunk.js"
+  },
+  {
+    "revision": "b0ce5035f71b375d9a6f",
+    "url": "/FaceDetectApp/static/js/runtime-main.1f970cf3.js"
+  }
+]);
